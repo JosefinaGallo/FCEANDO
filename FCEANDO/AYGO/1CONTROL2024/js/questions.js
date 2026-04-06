21bf9e80b45d3a6360eea61ce89b24c17d6ad7ce
JosefinaGallo <ezeg.pr@gmail.com>
2026-04-05 22:25:03 -0300

    Add files via upload

---
+++ FCEANDO/AYGO/1CONTROL2024/js/questions.js
@@ -0,0 +1,220 @@
+let questions = [
+{
+"numb": 1,
+"question": "La visión periférica consiste en la habilidad para:",
+"answer": ["b. Detectar las señales débiles que están fuera del área de focalización."],
+"options": [
+"a. Diseñar la planificación.",
+"b. Detectar las señales débiles que están fuera del área de focalización.",
+"c. Imaginar y crear el futuro."
+],
+"comment": "",
+},
+
+{
+"numb": 2,
+"question": "Con los roles y status la organización se asegura:",
+"answer": ["a. Que todos los individuos cumplan con sus obligaciones"],
+"options": [
+"a. Que todos los individuos cumplan con sus obligaciones",
+"b. Que cada individuo cumpla con su rol",
+"c. Sustituir una conducta espontánea por una esperada"
+],
+"comment": "",
+},
+
+{
+"numb": 3,
+"question": "Juan es el gerente de La Carola, establecimiento que se dedica al turismo, los últimos tiempos han sido muy complejos dadas las condiciones que ha traído la pandemia. Ante una coyuntura de este tipo podemos destacar que la meta básica de las organizaciones es:",
+"answer": ["c. la supervivencia"],
+"options": [
+"a. el desarrollo",
+"b. aprovechar las oportunidades",
+"c. la supervivencia"
+],
+"comment": "",
+},
+
+{
+"numb": 4,
+"question": "Los cambios se han producido en la gran mayoría de las variables: económicas, sociales, culturales. Si consideramos el índice de cambio:",
+"answer": ["a. La tecnología es la variable que presenta mayores cambios"],
+"options": [
+"a. La tecnología es la variable que presenta mayores cambios",
+"b. La tecnología presenta cambios que se ubican entre los cambios en los individuos y los cambios en los modelos de negocio",
+"c. La tecnología presenta cambios significativos pero menos a la de los modelos de negocio"
+],
+"comment": "",
+},
+
+{
+"numb": 5,
+"question": "El entorno específico de una organización:",
+"answer": ["b. Es único y cambia."],
+"options": [
+"a. Comprende las condiciones económicas, políticas, sociales y tecnológicas que pueden incidir en las organizaciones y que los gerentes deben tomar en cuenta cuando planifican, organizan, dirigen y controlar.",
+"b. Es único y cambia.",
+"c. Es todo lo que está fuera de la organización, y que puede afectar a la organización, pero cuya importancia no se aprecia con claridad."
+],
+"comment": "",
+},
+
+{
+"numb": 6,
+"question": "El grado de complejidad del ambiente hace referencia:",
+"answer": ["c. Al número de componentes del entorno y al grado de conocimiento de los mismos."],
+"options": [
+"a. Al grado en el cual el ambiente de una organización es estable o dinámico.",
+"b. A la insuficiencia de los enfoques tradicionales para afrontar los problemas.",
+"c. Al número de componentes del entorno y al grado de conocimiento de los mismos."
+],
+"comment": "",
+},
+
+{
+"numb": 7,
+"question": "El Sr. Ormachi es el Gerente General de una empresa filial de una marca internacional, que se dedica al armado de vehículos. El trabajo desarrollado en la planta de armado, se basa en estrictos protocolos de fabricación y el cumplimiento de altos estándares de calidad, controlados en los materiales y autopartes que se utilizan en el proceso. Los operarios tienen una capacitación constante y están altamente especializados en sus tareas específicas, dentro de determinados tramos del proceso productivo. Si bien existe un buen clima laboral e intercambio entre los empleados, se respeta siempre en último instancia, las decisiones de los niveles jerárquicos. <br> En función de la información suministrada, determine el tipo o modelo de organización de esta empresa.",
+"answer": ["b. Mecanicista"],
+"options": [
+"a. Organicista",
+"b. Mecanicista",
+"c. Vertical"
+],
+"comment": "",
+},
+
+{
+"numb": 8,
+"question": "De acuerdo a la encuesta realizada por PWC en el año 2016 el entorno futuro presentará las siguientes características:",
+"answer": ["c. será imposible realizar previsiones, aparecerán nuevos participantes, se requerirán otros enfoques para dar respuesta"],
+"options": [
+"a. la tecnología será clave en los procesos de cambio, lo que llevará a un cambio profundo de los vínculos",
+"b. se contará con nuevos actores y los actuales se transformarán",
+"c. será imposible realizar previsiones, aparecerán nuevos participantes, se requerirán otros enfoques para dar respuesta"
+],
+"comment": "",
+},
+
+
+{
+"numb": 9,
+"question": "Hace muchos años Monica, que se dedica a la prestación de servicios de consultoría asistió a una conferencia en la que se señalaba que la forma de comunicación de las personas cambiaría fuertemente, en ese entonces no se preocupo por actualizarse a nivel tecnológico porque entendió que esta mirada afectaría a otros sectores, ella conocía mucho a sus clientes. Hoy está muy preocupada y piensa que debería haber tenido en cuenta que la visión periférica nos permite:",
+"answer": ["c. estar preparado para hacer frente a la incertidumbre y poder actuar antes"],
+"options": [
+"a. imaginar un futuro y proyectarlo antes que otros",
+"b. ser eficiente en como se determinan las acciones futuras",
+"c. estar preparado para hacer frente a la incertidumbre y poder actuar antes"
+],
+"comment": "",
+},
+
+{
+"numb": 10,
+"question": "Marcos es el Gerente de Ventas de una importante empresa del rubro comida rápida. Se le ha planteado la oportunidad de abrir nuevos locales en el interior del país con la hipótesis de que existe una necesidad de mercado sin cubrir. <br> Ante esta posibilidad, y para obtener una interpretación más clara antes de tomar una decisión, ha conversado con otros colegas y clientes de la zona que conocen dicho mercado desde diferentes puntos de vista con el fin de no sesgar su opinión. Con respecto a las lecciones de la periferia:",
+"answer": ["b. esto confirma que antes de actuar hay que sondear es parte de la visión de periferia."],
+"options": [
+"a. esto brinda mayores oportunidades y enlentece el proceso de toma de decisiones, pero es parte de la visión de periferia.",
+"b. esto confirma que antes de actuar hay que sondear es parte de la visión de periferia.",
+"c. esto asegura un equilibrio en la visión periférica con la focal."
+],
+"comment": "",
+},
+
+
+{
+"numb": 11,
+"question": "La formalización como dimensión de la estructura se refiere:",
+"answer": ["a. Al grado en que los diferentes procesos de la organización se han predeterminado y deben ocurrir como fueron establecidos"],
+"options": [
+"a. Al grado en que los diferentes procesos de la organización se han predeterminado y deben ocurrir como fueron establecidos",
+"b. Al grado de dificultad en la realización de las tareas, grado de dificultad tecnológica",
+"c. Al proceso que transforma insumos en productos terminados"
+],
+"comment": "",
+},
+
+{
+"numb": 12,
+"question": "La generación Z se caracteriza porque sus miembros son:",
+"answer": ["c. Creativos y colaborativos, respetan la diversidad."],
+"options": [
+"a. Idealistas y competitivos.",
+"b. Buscan autoabastecerse y cuidar de sí mismos.",
+"c. Creativos y colaborativos, respetan la diversidad."
+],
+"comment": "",
+},
+
+{
+"numb": 13,
+"question": "¿Cuál de los grupos de acción no pertenecen al medio ambiente de entorno específico o directo?",
+"answer": ["b. Políticas de tributación del sector al que pertenece la organización."],
+"options": [
+"a. Gobiernos.",
+"b. Políticas de tributación del sector al que pertenece la organización.",
+"c. Sindicatos."
+],
+"comment": "",
+},
+
+{
+"numb": 14,
+"question": "Podemos decir que estamos ante una organización vigilante cuando esta:",
+"answer": ["a. cuenta con un proceso que le permite buscar, evaluar y responder a señales"],
+"options": [
+"a. cuenta con un proceso que le permite buscar, evaluar y responder a señales",
+"b. se concentra en la mirada futura de su propia organización",
+"c. releva en forma constante lo que está ocurriendo en empresas similares a las suyas"
+],
+"comment": "",
+},
+
+{
+"numb": 15,
+"question": "Los reglamentos son elementos característicos de:",
+"answer": ["a. La formalización"],
+"options": [
+"a. La formalización",
+"b. La centralización",
+"c. La complejidad"
+],
+"comment": "",
+},
+
+{
+"numb": 16,
+"question": "La empresa TERMIFLEX ha iniciado en 2022 una serie de acciones para obtener una certificación en calidad. En el marco de dichas acciones, se han comenzado a crear descripciones detalladas, plasmadas en manuales e instructivos, de todos los procesos y procedimientos de la organización. <br> ¿Qué dimensión de la estructura identifica?",
+"answer": ["a. Formalización"],
+"options": [
+"a. Formalización",
+"b. Centralización",
+"c. Complejidad"
+],
+"comment": "",
+},
+
+{
+"numb": 17,
+"question": "¿Qué elementos se tienen en cuenta al momento de realizar el diseño  rganizacional?",
+"answer": ["a. Estrategia, ambiente, tecnología y personas"],
+"options": [
+"a. Estrategia, ambiente, tecnología y personas",
+"b. Estructura deliberada, fines y voluntad de permanencia",
+"c. Formalización, centralización y tecnología"
+],
+"comment": "",
+},
+
+{
+"numb": 18,
+"question": "La empresa Beta SRL se encuentra preparando el cierre trimestral y para ello debe elaborar una serie de reportes. Uno de los auxiliares contables, Martín, está a cargo de la preparación de dos de dichos reportes. La fecha límite para su presentación es el tercer día hábil siguiente al final de cada mes. Este último mes, Martín ha presentado ambos reportes cumpliendo con la fecha prevista, pero tuvo que quedarse fuera de su horario habitual para lograrlo. En su opinión:",
+"answer": ["a. Ha sido eficaz, pero no eficiente"],
+"options": [
+"a. Ha sido eficaz, pero no eficiente",
+"b. Ha sido eficiente solamente",
+"c. Ha sido eficiente y eficaz"
+],
+"comment": "",
+},
+
+];
