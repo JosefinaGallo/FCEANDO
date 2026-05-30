0f5d020e17cb8605315e2c5ae95c4734ff8531f2
JosefinaGallo <ezeg.pr@gmail.com>
2026-05-30 17:21:32 -0300

    Add files via upload

---
+++ AYGO/2CONTROL2025/js/questions.js
@@ -0,0 +1,256 @@
+
+let questions = [
+{
+"numb": 1,
+"question": "Usted es el Gerente Comercial de la empresa ALFA. La empresa se dispone a lanzar al mercado un nuevo yogur, de su línea más económica para competir con el líder del mercado. Su objetivo es captar rápidamente una importante porción del mercado, a través de la venta de un volumen importante de unidades. ¿Qué estrategia de fijación de precios recomendaría?",
+"answer": ["b. Una estrategia de fijación de precios de penetración del mercado"],
+"options": [
+"a. Una estrategia de fijación de precios para capturar el nivel más alto del mercado",
+"b. Una estrategia de fijación de precios de penetración del mercado",
+"c. Una estrategia de fijación de precios basada en la competencia"
+],
+"comment": "",
+},
+
+{
+"numb": 2,
+"question": "La empresa ALFA ha encarado recientemente una campaña promocional para su nuevo producto el limpiador BRILLANTE. Como parte de la misma se está ofreciendo en los comercios un paquete conteniendo el limpiador BRILLANTE y el jabón en barra PUREZA, a un precio muy conveniente. Podemos decir que la herramienta de la mezcla promocional que se está usando es:",
+"answer": ["a. Paquete de precio global"],
+"options": [
+"a. Paquete de precio global",
+"b. Promoción en el punto de compra",
+"c. Bonificaciones"
+],
+"comment": "",
+},
+
+{
+"numb": 3,
+"question": "Los procesos muy flexibles capaces de ajustarse a las necesidades del cliente son llamados",
+"answer": ["c. unitarios"],
+"options": [
+"a. en lote",
+"b. lineales",
+"c. unitarios"
+],
+"comment": "",
+},
+
+{
+"numb": 4,
+"question": "La empresa “La Sin Rival S.A.” al inicio de cada ejercicio económico define un conjunto de objetivos que pretende lograr a lo largo del año, objetivos financieros, comerciales y productivos. Si al finalizar el año la empresa logra alcanzar los mismos se puede decir que “La Sin Rival S.A.” fue:",
+"answer": ["b. Eficaz."],
+"options": [
+"a. Productiva.",
+"b. Eficaz.",
+"c. Eficiente."
+],
+"comment": "",
+},
+
+{
+"numb": 5,
+"question": "Una empresa que fabrica comida para mascotas lanza una edición especial envasada en un contenedor rígido reutilizable, con tapa, que sirve también como almacenamiento. ¿De qué nivel del producto estamos hablando en este caso?",
+"answer": ["c. Producto aumentado"],
+"options": [
+"a. Producto real",
+"b. Producto básico",
+"c. Producto aumentado"
+],
+"comment": "",
+},
+
+{
+"numb": 6,
+"question": "Usted es Gerente Comercial de la empresa ALFA, que está a punto de iniciar una nueva campaña publicitaria. El Directorio le consulta acerca de la selección de los medios de difusión del mensaje, y en particular acerca de las características de los canales personales, y su respuesta es:",
+"answer": ["b. Permiten un contacto personal y una retroalimentación muy rápida!"],
+"options": [
+"a. No siempre el contacto es personal y la retroalimentación depende del cliente",
+"b. Permiten un contacto personal y una retroalimentación muy rápida!",
+"c. No permiten retroalimentación, a pesar del contacto personal"
+],
+"comment": "",
+},
+
+{
+"numb": 7,
+"question": "El flujo de producción es único cada vez en la producción",
+"answer": ["c. por proyecto"],
+"options": [
+"a. continua",
+"b. intermitente",
+"c. por proyecto"
+],
+"comment": "",
+},
+
+{
+"numb": 8,
+"question": "La banda COLONIA JAZZ ofrece espectáculos a voluntad en el caso histórico de Colonia del Sacramento. Se trata de un tipo de La banda COLONIA JAZZ ofrece espectáculos a voluntad en el caso histórico de Colonia del Sacramento. Se trata de un tipo de bien:",
+"answer": ["a. Intangible y efímero!"],
+"options": [
+"a. Intangible y efímero!",
+"b. Tangible y efímero",
+"c. Tangible y durable"
+],
+"comment": "",
+},
+
+{
+"numb": 9,
+"question": "La empresa Delta envía sus catálogos a los grandes compradores, a quienes vende directamente. En el caso del público en general, vende a minoristas que a su vez venden al público. Exclusivamente en base a estos datos podemos considerar que se trata de",
+"answer": ["b. Distribución multicanal!"],
+"options": [
+"a. Distribución a través de un canal indirecto.",
+"b. Distribución multicanal!",
+"c. Distribución a través de un canal directo"
+],
+"comment": "",
+},
+
+{
+"numb": 10,
+"question": "La empresa SOLUCIONES YA lanzó un nuevo producto de cuidado capilar con las mejores fórmulas e ingredientes que reducen la caída del cabello. Como tiene un tratamiento especial de colocación y cuidado del cabello, decidieron que el producto solo estará disponible en las peluquerías del país pero no se podrá vender en supermercados, farmacias u otros locales.",
+"answer": ["b. Distribución Selectiva"],
+"options": [
+"a. Distribución Exclusiva",
+"b. Distribución Selectiva",
+"c. Distribución Intensiva"
+],
+"comment": "",
+},
+
+{
+"numb": 11,
+"question": "En la etapa de Análisis del diseño del producto comercial se estudia",
+"answer": ["c. la funcionalidad esperada, las características sensoriales y la información incluida"],
+"options": [
+"a. los materiales y su calidad",
+"b. solo las características sensoriales y sus características técnicas",
+"c. la funcionalidad esperada, las características sensoriales y la información incluida"
+],
+"comment": "",
+},
+
+{
+"numb": 12,
+"question": "En el diagrama de flujo de trabajo o Diagrama de Procesos, el triángulo simboliza",
+"answer": ["a. el almacenamiento!"],
+"options": [
+"a. el almacenamiento!",
+"b. una inspección",
+"c. un transporte"
+],
+"comment": "",
+},
+
+{
+"numb": 13,
+"question": "La empresa Delta tradicionalmente vendió sus productos a un mayorista que a su vez le vendía a tres minoristas y éstos últimos al público en general. El mayorista recientemente ha decidido comenzar a venderle a dos nuevos minoristas, que se agregan a los que ya les vendía. Podemos por tanto afirmar que en cuanto a canales de distribución:",
+"answer": ["b. La empresa Delta no ha tenido cambios en cuanto a los niveles de su canal!"],
+"options": [
+"a. La empresa Delta ha agregado dos niveles a su canal indirecto.",
+"b. La empresa Delta no ha tenido cambios en cuanto a los niveles de su canal!",
+"c. La empresa Delta ha agregado dos niveles a su canal directo."
+],
+"comment": "",
+},
+
+{
+"numb": 14,
+"question": "Se aproximan las fiestas navideñas, por lo tanto, el gerente de producción de Brindiss ha planteado trabajar horas extras en fábrica. Eso significará aumentar la producción de champagne para afrontar la demanda esperada. Lógicamente esta decisión influye en el aumento de costo de personal y en la sobreutilización de las máquinas, situación que no puede ser mantenida por más de dos meses. Nos estamos refiriendo a:",
+"answer": ["c. Capacidad máxima"],
+"options": [
+"a. Decisiones de outsourcing",
+"b. Eficiencia productiva",
+"c. Capacidad máxima"
+],
+"comment": "",
+},
+
+{
+"numb": 15,
+"question": "La empresa ALFA es propietaria de varios cines en la ciudad de Montevideo. El precio común de las entradas es de $200. Para los estudiantes de secundaria es de $ 100, dado que la empresa quiere incentivar en los jóvenes el gusto por el cine. Exclusivamente en base a los datos anteriores podemos decir que la estrategia de ajuste de precios utilizada por la empresa ALFA para fijar el precio de las entradas es:",
+"answer": ["c. Fijación de precios segmentada"],
+"options": [
+"a. Fijación de precios geográfica",
+"b. Fijación de precios dinámica",
+"c. Fijación de precios segmentada"
+],
+"comment": "",
+},
+
+{
+"numb": 16,
+"question": "La empresa HAWKERS, con sede en Elche, España, se dedica a la venta de lentes de sol, y enfoca sus esfuerzos de marketing a personas jóvenes, entre 18 y 24 años, con intereses en la moda y las tendencias. El tipo de segmentación utilizada por HAWKERS es:",
+"answer": ["a. Demográfica y psicográfica!"],
+"options": [
+"a. Demográfica y psicográfica!",
+"b. Demográfica",
+"c. Geográfica"
+],
+"comment": "",
+},
+
+{
+"numb": 17,
+"question": "Una empresa que va a envasar refrescos decide la compra de una máquina de última generación para envasar la cantidad estimada a vender. Esto fue una decisión tomada que corresponde a:",
+"answer": ["a. Definición de la capacidad del proceso."],
+"options": [
+"a. Definición de la capacidad del proceso.",
+"b. Determinación de los niveles de inventario.",
+"c. Definición de las operaciones necesarias para la producción."
+],
+"comment": "",
+},
+
+{
+"numb": 18,
+"question": "La empresa ALFA ha obtenido la representación para Uruguay de la conocida marca de indumentaria juvenil CASUAL LOOK. Próximamente abrirán los tres primeros locales en Montevideo y se apuesta a una fuerte campaña de avisos en radio y televisión, así como a distribuir cupones para descuentos en prendas a los 150 primeros clientes. Podemos decir que las herramientas de la mezcla promocional que se están usando son:",
+"answer": ["a. Publicidad y promoción de ventas."],
+"options": [
+"a. Publicidad y promoción de ventas.",
+"b. Promoción de ventas y marketing directo.",
+"c. Promoción de ventas y ventas personales."
+],
+"comment": "",
+},
+
+{
+"numb": 19,
+"question": "La empresa “Mar Azul” se dedica a la importación y comercialización de productos de mar en el mercado local. Entre sus productos más vendidos se destacan los pescados blancos congelados y envasados al vacío que se ofrecen en las grandes cadenas de supermercados del país. La venta de los pescados blancos envasados al vacío es una decisión que refiere al:",
+"answer": ["b. Producto real."],
+"options": [
+"a. La distribución.",
+"b. Producto real.",
+"c. Producto básico."
+],
+"comment": "",
+},
+
+{
+"numb": 20,
+"question": "La empresa ALFA es propietaria de varias estaciones de servicio en Uruguay. El precio de la nafta común está fijado en $54 por litro, excepto en estaciones de localidades fronterizas del Chuy, Río Branco y Paysandú, donde es de $40 el litro. Para acceder a este precio, los clientes deben demostrar su residencia en dichas localidades. <br> Exclusivamente en base a los datos anteriores podemos decir que la estrategia de ajuste de precios utilizada por la empresa ALFA para fijar el precio de nafta común es:",
+"answer": ["a. Fijación de precios segmentada."],
+"options": [
+"a. Fijación de precios segmentada.",
+"b. Fijación de precios psicológica.",
+"c. Fijación de precios dinámica."
+],
+"comment": "",
+},
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
